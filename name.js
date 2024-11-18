e1f6cbfb1427a1be9698bba748107cbc9434a39f
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-18 16:40:28 +0100

    add-function & create import w/module.exports

--- name.js
+++ name.js
@@ -1,1 +1,12 @@
 console.log('server is running')
+
+const fullName = (firstName, lastName) => {
+    const userName = {
+      name: firstName,
+      surname: lastName,
+    };
+    return userName;
+  };
+  
+  module.exports = fullName;
+  
